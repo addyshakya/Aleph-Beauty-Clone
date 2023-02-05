128ef8d4aec6862db19227713a783ff93a141aba
aditya <shakyaaditya469@gmail.com>
2023-02-05 18:53:32 +0530

    first commit

---
+++ project38.js
@@ -0,0 +1,201 @@
+document.querySelector("#overlay1").addEventListener("mouseenter",function(){
+    document.querySelector("#overlay1 img").style.opacity=1
+    document.querySelector("#overlay1 #quick").style.bottom="0%"
+
+  })
+
+  document.querySelector("#overlay1").addEventListener("mouseleave",function(){
+    document.querySelector("#overlay1 img").style.opacity=0
+    document.querySelector("#overlay1 #quick").style.bottom="-10%"
+  })
+
+
+  document.querySelector("#overlay2").addEventListener("mouseenter",function(){
+    document.querySelector("#overlay2 img").style.opacity=1
+    document.querySelector("#overlay2 #quick").style.bottom="0%"
+
+  })
+
+  document.querySelector("#overlay2").addEventListener("mouseleave",function(){
+    document.querySelector("#overlay2 img").style.opacity=0
+    document.querySelector("#overlay2 #quick").style.bottom="-10%"
+  })
+
+  document.querySelector("#overlay3").addEventListener("mouseenter",function(){
+    document.querySelector("#overlay3 img").style.opacity=1
+    document.querySelector("#overlay3 #quick").style.bottom="0%"
+
+  })
+
+  document.querySelector("#overlay3").addEventListener("mouseleave",function(){
+    document.querySelector("#overlay3 img").style.opacity=0
+    document.querySelector("#overlay3 #quick").style.bottom="-10%"
+  })
+
+document.querySelector("#first").addEventListener("click",function(){
+    document.querySelector("#face").style.color = "grey"
+    document.querySelector("#first").style.color = "black"
+    document.querySelector("#eyes").style.color = "grey"
+    document.querySelector("#over").style.opacity = 1
+    document.querySelector("#photu").style.opacity = 1
+    document.querySelector("#over2").style.opacity = 0
+    document.querySelector("#photu2").style.opacity = 0
+    document.querySelector("#over3").style.opacity = 0
+    document.querySelector("#photu3").style.opacity = 0
+})
+
+
+document.querySelector("#face").addEventListener("click",function(){
+    document.querySelector("#face").style.color = "black"
+    document.querySelector("#first").style.color = "grey"
+    document.querySelector("#eyes").style.color = "grey"
+    document.querySelector("#over").style.opacity = 0
+    document.querySelector("#photu").style.opacity = 0
+    document.querySelector("#over2").style.opacity = 1
+    document.querySelector("#photu2").style.opacity = 1
+    document.querySelector("#over3").style.opacity = 0
+    document.querySelector("#photu3").style.opacity = 0
+})
+
+document.querySelector("#eyes").addEventListener("click",function(){
+    document.querySelector("#face").style.color = "grey"
+    document.querySelector("#first").style.color = "grey"
+    document.querySelector("#eyes").style.color = "black"
+    document.querySelector("#over").style.opacity = 0
+    document.querySelector("#photu").style.opacity = 0
+    document.querySelector("#over2").style.opacity = 0
+    document.querySelector("#photu2").style.opacity = 0
+    document.querySelector("#over3").style.opacity = 1
+    document.querySelector("#photu3").style.opacity = 1
+})
+
+document.querySelector("#overlay21").addEventListener("mouseenter",function(){
+    document.querySelector("#overlay21 img").style.opacity=1
+    document.querySelector("#overlay21 #quick").style.bottom="0%"
+
+  })
+
+  document.querySelector("#overlay21").addEventListener("mouseleave",function(){
+    document.querySelector("#overlay21 img").style.opacity=0
+    document.querySelector("#overlay21 #quick").style.bottom="-10%"
+  })
+
+  document.querySelector("#overlay22").addEventListener("mouseenter",function(){
+    document.querySelector("#overlay22 img").style.opacity=1
+    document.querySelector("#overlay22 #quick").style.bottom="0%"
+
+  })
+
+  document.querySelector("#overlay22").addEventListener("mouseleave",function(){
+    document.querySelector("#overlay22 img").style.opacity=0
+    document.querySelector("#overlay22 #quick").style.bottom="-10%"
+  })
+
+  document.querySelector("#overlay23").addEventListener("mouseenter",function(){
+    document.querySelector("#overlay23 img").style.opacity=1
+    document.querySelector("#overlay23 #quick").style.bottom="0%"
+
+  })
+
+  document.querySelector("#overlay23").addEventListener("mouseleave",function(){
+    document.querySelector("#overlay23 img").style.opacity=0
+    document.querySelector("#overlay23 #quick").style.bottom="-10%"
+  })
+
+
+document.querySelector("#overlay31").addEventListener("mouseenter",function(){
+    document.querySelector("#overlay31 img").style.opacity=1
+    document.querySelector("#overlay31 #quick").style.bottom="0%"
+
+  })
+
+  document.querySelector("#overlay31").addEventListener("mouseleave",function(){
+    document.querySelector("#overlay31 img").style.opacity=0
+    document.querySelector("#overlay31 #quick").style.bottom="-10%"
+  })
+
+  document.querySelector("#overlay32").addEventListener("mouseenter",function(){
+    document.querySelector("#overlay32 img").style.opacity=1
+    document.querySelector("#overlay32 #quick").style.bottom="0%"
+
+  })
+
+  document.querySelector("#overlay32").addEventListener("mouseleave",function(){
+    document.querySelector("#overlay32 img").style.opacity=0
+    document.querySelector("#overlay32 #quick").style.bottom="-10%"
+  })
+
+  document.querySelector("#overlay33").addEventListener("mouseenter",function(){
+    document.querySelector("#overlay33 img").style.opacity=1
+    document.querySelector("#overlay33 #quick").style.bottom="0%"
+
+  })
+
+  document.querySelector("#overlay33").addEventListener("mouseleave",function(){
+    document.querySelector("#overlay33 img").style.opacity=0
+    document.querySelector("#overlay33 #quick").style.bottom="-10%"
+  })
+
+document.querySelector("#I1").addEventListener("click", function(){
+  document.querySelector("#Q1").style.opacity = 1
+  document.querySelector("#Q2").style.opacity = 0
+  document.querySelector("#Q3").style.opacity = 0
+  document.querySelector("#Q4").style.opacity = 0
+  document.querySelector("#Q5").style.opacity = 0
+})
+
+document.querySelector("#I2").addEventListener("click", function(){
+  document.querySelector("#Q1").style.opacity = 0
+  document.querySelector("#Q2").style.opacity = 1
+  document.querySelector("#Q3").style.opacity = 0
+  document.querySelector("#Q4").style.opacity = 0
+  document.querySelector("#Q5").style.opacity = 0
+})
+
+document.querySelector("#I3").addEventListener("click", function(){
+  document.querySelector("#Q1").style.opacity = 0
+  document.querySelector("#Q2").style.opacity = 0
+  document.querySelector("#Q3").style.opacity = 1
+  document.querySelector("#Q4").style.opacity = 0
+  document.querySelector("#Q5").style.opacity = 0
+})
+
+document.querySelector("#I4").addEventListener("click", function(){
+  document.querySelector("#Q1").style.opacity = 0
+  document.querySelector("#Q2").style.opacity = 0
+  document.querySelector("#Q3").style.opacity = 0
+  document.querySelector("#Q4").style.opacity = 1
+  document.querySelector("#Q5").style.opacity = 0
+})
+
+document.querySelector("#I5").addEventListener("click", function(){
+  document.querySelector("#Q1").style.opacity = 0
+  document.querySelector("#Q2").style.opacity = 0
+  document.querySelector("#Q3").style.opacity = 0
+  document.querySelector("#Q4").style.opacity = 0
+  document.querySelector("#Q5").style.opacity = 1
+})
+
+document.querySelector("#e2").addEventListener("mouseenter" , function(){
+  document.querySelector("#e2>button").style.opacity =1
+})
+
+document.querySelector("#e2").addEventListener("mouseleave" , function(){
+  document.querySelector("#e2>button").style.opacity =0
+})
+
+document.querySelector("#e3").addEventListener("mouseenter" , function(){
+  document.querySelector("#e3>button").style.opacity =1
+})
+
+document.querySelector("#e3").addEventListener("mouseleave" , function(){
+  document.querySelector("#e3>button").style.opacity =0
+})
+
+document.querySelector("#e4").addEventListener("mouseenter" , function(){
+  document.querySelector("#e4>button").style.opacity =1
+})
+
+document.querySelector("#e4").addEventListener("mouseleave" , function(){
+  document.querySelector("#e4>button").style.opacity =0
+})
